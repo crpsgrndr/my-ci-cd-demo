922884cc7dbbb1adb859fe760e6cc69a7caf5b65
crpsgrndr <euaxlura@gmail.com>
2024-12-12 22:57:29 +0800

    Add CI/CD pipeline with GitHub Actions

---
+++ __tests__/index.test.js
@@ -0,0 +1,9 @@
+const add = require('../index');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('adds -1 + -1 to equal -2', () => {
+    expect(add(-1, -1)).toBe(-2);
+});
